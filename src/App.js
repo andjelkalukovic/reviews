9773012e39255fee5c4d3a6f4e4658820248814f
Andjelka Lukovic <andjelkal@live.com>
2021-03-04 18:02:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Review from "./Review";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main-review align-self-center'>
+      <section>
+        <div className='container text-center'>
+          <h1 className='pb-4'>Reviews</h1>
+          <Review />
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/Review.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import people from './data';
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
+import {BsCaretLeft, BsCaretRight} from 'react-icons/bs'
+import {ImQuotesRight} from "react-icons/im";
+
+const Review = () => {
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text } = people[index];
+
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0
+        }
+        if (number < 0) {
+            return people.length - 1
+        }
+        return number;
+    }
+
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkNumber(newIndex);
+        });
+    }
+
+    const prevPerson = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            return checkNumber(newIndex);
+        });
+    }
+
+    const randomPerson = () => {
+        let randomNumber = Math.floor(Math.random() * people.length);
+        if (randomNumber === index) {
+            randomNumber = index + 1
+        }
+        setIndex(checkNumber(randomNumber))
+    }
+
+    return (
+        <article className='row justify-content-center'>
+            <div className='one-review py-4 col-lg-10 justify-center'>
+                <div>
+                    <div>
+                        <i className='qoutes'><ImQuotesRight /></i>
+                    </div>
+                    <Image src={image} alt={name} roundedCircle />
+                </div>
+
+                <div>
+                    <h4 className='text-capitalize font-weight-bolder'>{name}</h4>
+                    <p className='text-uppercase text-info'>{job}</p>
+                    <p className='text-center px-5'>{text}</p>
+                </div>
+
+                <div>
+                    <a onClick={prevPerson} className='icon px-2'><BsCaretLeft /></a>
+                    <a onClick={nextPerson} className='icon px-2'><BsCaretRight /></a>
+                </div>
+                <Button variant="outline-info" onClick={randomPerson} className='mt-3'>Surprise me</Button>{' '}
+            </div>
+        </article>
+    )
+}
+
+export default Review;
